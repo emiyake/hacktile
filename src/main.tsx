2107606aa16a80e8a13cec847237956e6529cfa3
RafaelFS <rfl.fsilva@gmail.com>
2017-06-14 19:57:04 -0300

    Adds a very simple login wireframe screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,71 @@
-import React, { Component } from 'react';
 import {
+  Button,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from 'react-native';
+import React, { Component } from 'react';
 
 import { Timer } from './timer';
 
 export default class App extends Component<any, any> {
 
+  onLoginPressed() {
+    console.log(this.state);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Hello World!!!!!
+
+        <Text style={styles.title}>
+          Login
         </Text>
-        <Text style={styles.instructions}>
-          To get started, modify main.tsx
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload on iOS or R+R in Android,{'\n'}
-          Press Cmd+D on iOS, Cmd+M on Android or shake for dev menu
-        </Text>
+
+        <Text style={styles.inputLabel}>E-mail</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType={'email-address'}
+          autoCapitalize={'none'}
+          onChangeText={(text) => this.setState({email: text})} />
+
+        <Text style={styles.inputLabel}>Password</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          onChangeText={(text) => this.setState({password: text})} />
+
+        <Button title='Entrar' onPress={() => this.onLoginPressed()}/>
+
         <Timer />
+
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  inputLabel: {
+    paddingLeft: 8,
+    paddingRight: 8,
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    margin: 8,
+    padding: 8,
   },
 });
+
+

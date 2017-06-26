75f8cc7be86127d5f0b13e1360f22a9f51557724
RafaelFS <rfl.fsilva@gmail.com>
2017-06-26 19:52:31 -0300

    Updates Apollo code gen and makes it recognize exported queries

--- src/app/models/graphql.schema.ts
+++ src/app/models/graphql.schema.ts
@@ -1,3 +1,13 @@
-//  This file was automatically generated and should not be edited.
 /* tslint:disable */
+//  This file was automatically generated and should not be edited.
+
+export type SpecialtyListQuery = {
+  Specialties:  {
+    // Array of elements
+    nodes:  Array< {
+      // Specialty name
+      name: string | null,
+    } > | null,
+  } | null,
+};
 /* tslint:enable */
